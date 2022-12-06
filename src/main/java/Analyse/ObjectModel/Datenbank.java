323e45fc5df131992db83881b272feb060864d5f
Lukas <Lukasr101@gmail.com>
2022-12-06 18:39:49 +0100

    finializing sys-ops

---
+++ src/main/java/Analyse/ObjectModel/Datenbank.java
@@ -0,0 +1,13 @@
+package Analyse.ObjectModel;
+public class Datenbank {
+
+
+
+	public void handicapAbfragen() {
+		return;
+	}
+
+	public void kategorienLaden() {
+		return;
+	}
+}
--- src/main/java/Analyse/ObjectModel/Spiel.java
+++ src/main/java/Analyse/ObjectModel/Spiel.java
@@ -8,4 +8,57 @@ public class Spiel {
 	 * @supplierCardinality 1 
 	 */
 	private Analyse.ObjectModel.Spielfeld lnkSpielfeld;
+
+
+
+
+
+
+
+
+
+
+	public void figurBewegen() {
+		return;
+	}
+
+
+	public void figurAufStartfeldBewegen() {
+		return;
+	}
+
+
+	public void wuerfelnSpielzug() {
+		return;
+	}
+
+
+	public void wuefeln() {
+		return;
+	}
+
+
+	public void spielerErstellen() {
+		return;
+	}
+
+
+	public void erneutWuerfeln() {
+		return;
+	}
+
+
+	public void kategorienAuswaehlen() {
+		return;
+	}
+
+
+	public void weiter() {
+		return;
+	}
+
+
+	public void spielerAnlegen() {
+		return;
+	}
 }
